1976784198cc4d97a3b049be897d52fa97c17322
Duc Dao Huy <huyduc@hocvienact.edu.vn>
2018-06-27 14:58:45 +0700

    web act

---
+++ js/slick.js
@@ -0,0 +1,55 @@
+(function(document, window, $) {
+	$(document).ready(function() {
+		$('.slider-slick1').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			prevArrow : '.alight .prev',
+			nextArrow : '.alight .next',
+		});
+	});
+
+	$(document).ready(function() {
+		$('.slider-slick2, .slider-slick3').slick({
+			slidesToShow: 4
+		});
+	});
+
+	
+	$(document).ready(function() {
+		$('.slider-slick4, .slider-slick6').slick({
+			slidesToShow: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			prevArrow : null,
+			nextArrow : null,
+			dots: false,
+  			infinite: true,
+			fade: true,
+			cssEase: 'linear'
+		});
+	});
+
+	$(document).ready(function() {
+		$('.slider-slick5').slick({
+			slidesToShow: 5,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			prevArrow : null,
+			nextArrow : null,
+		});
+	});
+
+	$(document).ready(function() {
+		$('.slider-slick7').slick({
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000,
+			prevArrow : '.alight .prev1',
+			nextArrow : '.alight .next1',
+		});
+	});
+}) (document, window, jQuery);
